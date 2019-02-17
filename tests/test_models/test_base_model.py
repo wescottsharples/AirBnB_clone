901b90f8d2a54dea2dfe7bd726172991a3fa118c
Athena Deng <539@holbertonschool.com>
2019-02-17 21:09:39 +0000

    added test for __str__ method, setUp and tearDown for storage to file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python3
 """Unittests for BaseModel"""
 import datetime
+import os
 import unittest
 from models.base_model import BaseModel
 
 
 class TestClassBaseModel(unittest.TestCase):
     """test the BaseModel class"""
+    def setUp(self):
+        """if file.json exists, copy contents to temp file"""
+        if os.path.isfile('./file.json') is True:
+            with open("file.json") as r:
+                instances = r.read()
+            with open("temp.json", 'w+') as w:
+                w.write(instances)
+
+    def tearDown(self):
+        """
+        if temp file exists, copy contents back to file.json and
+        remove temp file
+        """
+        if os.path.isfile('./temp.json') is True:
+            with open("temp.json") as r:
+                instances = r.read()
+            with open("file.json", 'w') as w:
+                w.write(instances)
+            os.remove("temp.json")
+        elif os.path.isfile('./file.json') is True:
+            os.remove("file.json")
+
     def test_new_instance(self):
         """tests creating a new instance of BaseModel class"""
         testb = BaseModel()
@@ -24,6 +47,14 @@ class TestClassBaseModel(unittest.TestCase):
         self.assertTrue(update_t != saved_update_t)
         self.assertTrue("file.json")
 
+    def test_str(self):
+        """tests __str__ method of BaseModel class"""
+        testb = BaseModel()
+        testb_str = testb.__str__()
+        testb_str = testb_str.split()
+        self.assertEqual(testb_str[0], "[{}]".format(testb.__class__.__name__))
+        self.assertEqual(testb_str[1], "({})".format(testb.id))
+
     def test_to_dict(self):
         """tests the to_dict method in BaseModel class"""
         testb = BaseModel()
@@ -33,3 +64,6 @@ class TestClassBaseModel(unittest.TestCase):
         update_t = testb_dict['updated_at']
         self.assertTrue(type(create_t) is str)
         self.assertTrue(type(update_t) is str)
+
+if __name__ == '__main__':
+    unittest.main()
