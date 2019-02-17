a3aba2aa84be6ccb76f19e839e5a89ebc751b4c4
Athena Deng <539@holbertonschool.com>
2019-02-17 05:00:22 +0000

    incomplete work

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """Unittests for storage engine"""
+import unittest
+from models.engine.file_storage import FileStorage
 
 
-
+class TestClassFileStorage(unittest.TestCase):
+    """test the FileStorage class"""
+    def test_all(self):
+        """tests the all method"""
+        pass
