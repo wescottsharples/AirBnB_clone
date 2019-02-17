044f9ef9202ad56fd5f1188033a0fc3d1aa6f6fd
Athena Deng <539@holbertonschool.com>
2019-02-17 21:40:11 +0000

    incomplete work

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python3
 """Unittests for Review"""
+import datetime
+import os
+import unittest
+from models.base_model import BaseModel
 
 
+class TestClassReview(unittest.TestCase):
+    """unittests for Review class"""
+    def setUp(self):
+        """if file.json exists, copy contents to temp file"""
+        if os.path.isfile('./file.json') is True:
+            with open("file.json") as r:
+                instances = r.read()
+            with open("temp.json", 'w+') as w:
+                w.write(instances)
 
+    def tearDown(self):
+        """
+        if temp file exists, copy contents back to file.json and
+        remove temp file
+        """
+        if os.path.isfile('./temp.json') is True:
+            with open("temp.json") as r:
+                instances = r.read()
+            with open("file.json", 'w') as w:
+                w.write(instances)
+            os.remove("temp.json")
+        elif os.path.isfile('./file.json') is True:
+            os.remove("file.json")
+
+    def test_new_instance(self):
+       """tests  
