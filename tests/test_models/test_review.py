e8f233e8896188f17ce463932ebb88f423a37584
Athena Deng <539@holbertonschool.com>
2019-02-18 03:13:59 +0000

    unittests for methods and attributes

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -3,7 +3,7 @@
 import datetime
 import os
 import unittest
-from models.base_model import BaseModel
+from models.review import Review
 
 
 class TestClassReview(unittest.TestCase):
@@ -31,4 +31,59 @@ class TestClassReview(unittest.TestCase):
             os.remove("file.json")
 
     def test_new_instance(self):
-       """tests  
+        """tests creating a new instance of Review"""
+        testr = Review()
+        self.assertTrue(type(testr) is Review)
+        self.assertTrue(type(testr.id) is str)
+        self.assertTrue(type(testr.created_at) is datetime.datetime)
+        self.assertTrue(type(testr.updated_at) is datetime.datetime)
+        self.assertTrue(type(testr.place_id) is str)
+        self.assertTrue(len(testr.place_id) == 0)
+        self.assertTrue(type(testr.user_id) is str)
+        self.assertTrue(len(testr.user_id) == 0)
+        self.assertTrue(type(testr.text) is str)
+        self.assertTrue(len(testr.text) == 0)
+
+    def test_save(self):
+        """tests save method inherited from BaseModel"""
+        testr = Review()
+        update_t = testr.updated_at.isoformat(sep='T')
+        testr.save()
+        saved_update_t = testr.updated_at.isoformat(sep='T')
+        self.assertTrue(update_t != saved_update_t)
+        self.assertTrue("file.json")
+
+    def test_str(self):
+        """tests __str__ method inherited from BaseModel"""
+        tr = Review()
+        tr_str = tr.__str__()
+        tr_strl = tr_str.split()
+        self.assertEqual(tr_strl[0], "[{}]".format(tr.__class__.__name__))
+        self.assertEqual(tr_strl[1], "({})".format(tr.id))
+
+    def test_to_dict(self):
+        """tests the to_dict method inherited from BaseModel"""
+        testr = Review()
+        testr_dict = testr.to_dict()
+        self.assertTrue(type(testr_dict) is dict)
+        create_t = testr_dict['created_at']
+        update_t = testr_dict['updated_at']
+        self.assertTrue(type(create_t) is str)
+        self.assertTrue(type(update_t) is str)
+
+    def test_recreate(self):
+        """tests creating an instance of Review using **kwargs"""
+        new_obj = Review()
+        obj_dict = new_obj.to_dict()
+        recreate_obj = Review(**obj_dict)
+        r_obj_dict = recreate_obj.to_dict()
+        self.assertTrue(type(recreate_obj) is Review)
+        self.assertTrue(type(recreate_obj.id) is str)
+        self.assertTrue(type(recreate_obj.created_at) is datetime.datetime)
+        self.assertTrue(type(recreate_obj.updated_at) is datetime.datetime)
+        self.assertEqual(type(new_obj), type(recreate_obj))
+        self.assertEqual(new_obj.id, recreate_obj.id)
+        self.assertEqual(len(obj_dict), len(r_obj_dict))
+
+if __name__ == '__main__':
+    unittest.main()
