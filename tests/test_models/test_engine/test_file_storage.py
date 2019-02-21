f31f815e582544c7d8ad755a432587fc161f6e83
Athena Deng <539@holbertonschool.com>
2019-02-21 00:32:49 +0000

    added tests for save and reload methods of FileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -39,12 +39,33 @@ class TestClassFileStorage(unittest.TestCase):
     def test_new_save(self):
         """tests the new and save methods in FileStorage class"""
         temp_storage = FileStorage()
+        not_py_dict = "Hello world"
+        with open('file.json', 'w') as f:
+            f.write(not_py_dict)
+        with open('file.json') as r:
+            test_str = r.read()
         testb = BaseModel()
         temp_storage.new(testb)
         testb_key = "{}.{}".format(testb.__class__.__name__, testb.id)
         self.assertTrue(testb_key in temp_storage.all().keys())
         temp_storage.save()
         self.assertTrue('file.json')
+        with open('file.json') as f:
+            json_contents = f.read()
+        self.assertTrue(not_py_dict not in json_contents)
+
+    def test_reload(self):
+        """tests the reload method in FileStorage class"""
+        temp_storage = FileStorage()
+        not_py_dict = "Hello world"
+        testb = BaseModel()
+        temp_storage.save()
+        temp_storage.reload()
+        self.assertTrue(len(temp_storage.all()) > 0)
+        with open('file.json', 'w') as f:
+            f.write(not_py_dict)
+        with self.assertRaises(Exception):
+            temp_storage.reload()
 
 if __name__ == '__main__':
     unittest.main()
